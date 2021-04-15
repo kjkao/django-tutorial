b3ed79949f72fc2cfa353316644e7fd751b58b2e
K.J. Kao <jeremy.kao@gmail.com>
2021-04-15 16:39:01 +0800

    Django 教學 2: 創建一個骨架網站
    https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Django/skeleton_website
    
    - python3 manage.py startapp catalog
    - python3 manage.py makemigrations
    - python3 manage.py migrate

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='polls/')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('catalog/', include('catalog.urls')),
 ]
