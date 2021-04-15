5b7f93609c1c4d759498e0b14d99617cc059c557
K.J. Kao <jeremy.kao@gmail.com>
2021-04-15 16:55:39 +0800

    Django Tutorial Part 5: Creating our home page
    https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Django/Home_page

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='polls/')),
+    path('', RedirectView.as_view(url='catalog/')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('catalog/', include('catalog.urls')),
