d1a048c63bc9c83e654b37d3e76f07958be3550f
K.J. Kao <jeremy.kao@gmail.com>
2021-04-15 16:05:47 +0800

    first Django app, part 7
    https://docs.djangoproject.com/en/3.2/intro/tutorial07/

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin_0(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+class QuestionAdmin_1(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class ChoiceInline_1(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
